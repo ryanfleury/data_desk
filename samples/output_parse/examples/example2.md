f95ec0c838f71555a5d7cb343f1e089c19832c20
Phil <homan.phil@gmail.com>
2021-01-13 16:49:28 -0800

    Add a sample program that outputs the parse infor for each input file. Maybe this would make more sense under a tools folder?

---
+++ samples/output_parse/examples/example2.md
@@ -0,0 +1,19 @@
+@sets a_set : {
+    named_set : {1, 3, 7},
+	{not, named, set}
+	@tag_with_params(p1, p2, p3) { e1, e2, e3 },
+}
+
+@empty_set empty : {
+}
+
+@tagged_unnamed_set {
+	a_label: {unnamed_set_element}
+}
+
+@showcase_mixed_set_scoping {
+	@symbol_label
+	+: (1 2 3],
+	@symbol_label
+	*: [x y z),
+}
---
+++ samples/output_parse/output_parse.c
@@ -0,0 +1,103 @@
+// Sample code to print out the data from MD_ParseWholeFile
+#include "md.h"
+#include "md.c"
+
+#define ARRAY_COUNT(Array) (sizeof(Array) / sizeof((Array)[0]))
+
+// NOTE(pmh): This must be kept up to date with MD_NodeFlags in md.h
+const char* flag_names[] = {
+    "ParenLeft", "ParenRight", "BracketLeft", "BracketRight", "BraceLeft", "BraceRight",
+    "BeforeSemicolon", "BeforeComma",
+    "AfterSemicolon", "AfterComma",
+    "Numeric", "Identifier", "StringLiteral", "CharLiteral"
+};
+
+// NOTE: this must be kept up to date with MD_NodeKind in md.h
+const char* node_kinds[] = {
+    "Nil", "File", "Label", "UnnamedSet", "Tag", "MAX"
+};
+
+#define INDENT_SPACES 4
+static void PrintNode(MD_Node* node, FILE* file, int indent_count) {
+    char* indent_str = _MD_PushArray(_MD_GetCtx(), char, indent_count*INDENT_SPACES+1);
+    for (int i = 0; i < indent_count*INDENT_SPACES; i++) {
+        indent_str[i] = ' ';
+    }
+    indent_str[indent_count*INDENT_SPACES] = '\0';
+    
+    fprintf(file, "%sNode{\n", indent_str);
+    fprintf(file, "%s    Kind: %s,\n", indent_str, node_kinds[node->kind]);
+    
+    int flags_bits = sizeof(node->flags)*8;
+    char binary_flags[sizeof(node->flags)*8+1];
+    binary_flags[flags_bits] = '\0';
+    int flag_index = 0;
+    MD_u32 flags = node->flags;
+    for (int i = 0; i < flags_bits; i++) {
+        binary_flags[i] = (flags&1) ? '1' : '0';
+        flag_index++;
+        flags >>= 1;
+    }
+    
+    fprintf(file, "%s    Flags: %s,\n", indent_str, binary_flags);
+    fprintf(file, "%s    Flag Names: ", indent_str, binary_flags);
+    for (int i = 0; i < ARRAY_COUNT(flag_names); i++) {
+        if (binary_flags[i] == '1') {
+            fprintf(file, "%s, ", flag_names[i]);
+        }
+    }
+    fprintf(file, "\n");
+    if(node->string.size > 0) fprintf(file, "%s    String: %.*s,\n", indent_str, MD_StringExpand(node->string));
+    if(node->whole_string.size > 0) fprintf(file, "%s    Whole String: %.*s,\n", indent_str, MD_StringExpand(node->whole_string));
+    if (node->first_tag->kind != MD_NodeKind_Nil) {
+        fprintf(file, "%s    Tags: ", indent_str);
+        for (MD_EachNode(tag, node->first_tag)) {
+            fprintf(file, "@%.*s, ", MD_StringExpand(tag->string));
+        }
+        fprintf(file, "\n");
+    }
+    
+    for(MD_EachNode(child, node->first_child)) {
+        PrintNode(child, file, indent_count+1);
+    }
+    fprintf(file, "%s}\n", indent_str);
+}
+
+int main(int argument_count, char **arguments)
+{
+    if (ARRAY_COUNT(node_kinds)-1 != MD_NodeKind_MAX) {
+        printf("node_kinds strings array length-1 does not match MD_NodeKind_MAX. Aborting.\n");
+        printf("%I64u != %i\n", ARRAY_COUNT(node_kinds)-1, MD_NodeKind_MAX);
+        return;
+    }
+    
+    // NOTE(pmh): Parse all the files passed in via command line.
+    MD_Node *first = MD_NilNode();
+    MD_Node *last = MD_NilNode();
+    for(int i = 1; i < argument_count; i += 1)
+    {
+        MD_Node *root = MD_ParseWholeFile(MD_S8CString(arguments[i]));
+        MD_PushSibling(&first, &last, MD_NilNode(), root);
+    }
+    
+    for(MD_EachNode(root, first))
+    {
+        MD_String8 filename = MD_TrimExtension(MD_TrimFolder(root->filename));
+        MD_String8List str_list = {0};
+        MD_PushStringToList(&str_list, MD_S8CString("parsed_"));
+        MD_PushStringToList(&str_list, filename);
+        MD_PushStringToList(&str_list, MD_S8CString(".txt"));
+        char* out_file = (char*)MD_PushStringCopy(MD_JoinStringList(str_list)).str;
+        printf("Parse Input -> Output: %s -> %s\n", root->filename.str, out_file);
+        
+        FILE* file = fopen(out_file, "w");
+        for(MD_EachNode(node, root->first_child))
+        {
+            PrintNode(node, file, 0);
+            fprintf(file, "\n");
+        }
+        fclose(file);
+    }
+    
+    return 0;
+}
